b6c922cdeb31b9e5c7d19144f4d013138b3e7030
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-17 20:54:32 +0600

    complet travel cost calclution

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-function getQuantity(idName,isMars,isAdd){
-    const inputFeild = document.getElementById(idName);
-    inputFeildValue = parseInt(inputFeild.value);
-    if(isAdd){
-      inputFeildValue ++;
-    }else{
-      if(inputFeildValue >= 1){
-        inputFeildValue --;
-      }  
-    } 
-   inputFeild.value = inputFeildValue;
-//   const quantity = getQuantity(idName, isAdd)
-   const netTiketCost = getNetMarsTiketCost(quantity, isMars);
-   setTiketCost(netTiketCost, isMars);
-   getTotalCost();
-  }
-
+ /* ==================
+  event handelar 
+==================== */
+function getQuantityHandelar(idName, isMars, isAdd) {
+    const quantity = getQuantity(idName, isAdd);
+    const netTiketCost = getNetMarsTiketCost(quantity, isMars);
+    setTiketCost(netTiketCost, isMars);
+    getTotalCost();
+}
+/* ====================
+ get quantity 
+====================== */
 function getQuantity(idName, isAdd){
     const inputFeild = document.getElementById(idName);
     let quantity = parseInt(inputFeild.value);
@@ -28,28 +23,35 @@ function getQuantity(idName, isAdd){
     inputFeild.value = quantity;
     return quantity;
 }
-  // net tiketCost 
-  function getNetMarsTiketCost(quantity, isMars){
-    let perTiketCost;
-    if(isMars){
-       perTiketCost = 500;
-    }else{
-       perTiketCost = 300;
-    }
-    const netCost = quantity *perTiketCost
-      return netCost;
-  }
-  // set tiketCost 
-  function setTiketCost(tiketCost, isMars){
-    if(isMars){
-      document.getElementById('mars-cost').innerText = tiketCost;
-    }else{
-      document.getElementById('moon-cost').innerText = tiketCost;
-    }
-  }
 
-  // set total cost 
-  function getTotalCost(){
-    const totalCost = parseInt(document.getElementById('mars-cost').innerText) + parseInt(document.getElementById('moon-cost').innerText );
+ /* ====================
+  net tiketCost 
+ ===================== */
+function getNetMarsTiketCost(quantity, isMars) {
+    let perTiketCost;
+    if (isMars) {
+        perTiketCost = 500;
+    } else {
+        perTiketCost = 300;
+    }
+    const netCost = quantity * perTiketCost
+    return netCost;
+}
+/* ===================
+ set tiketCost 
+===================== */
+function setTiketCost(tiketCost, isMars) {
+    if (isMars) {
+        document.getElementById('mars-cost').innerText = tiketCost;
+    } else {
+        document.getElementById('moon-cost').innerText = tiketCost;
+    }
+}
+
+/* ==================
+set total cost  
+=================== */
+function getTotalCost() {
+    const totalCost = parseInt(document.getElementById('mars-cost').innerText) + parseInt(document.getElementById('moon-cost').innerText);
     document.getElementById('total-cost').innerText = totalCost;
-  }
+}
